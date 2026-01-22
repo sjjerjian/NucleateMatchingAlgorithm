d7f3583cbe929fa4d89c3a2884bb99e068a5c763
Steven Jerjian <sjerjian@jhu.edu>
2026-01-22 07:47:05 -0500

    [ENH] break into functions

--- scripts/mentor_team_matching.py
+++ scripts/mentor_team_matching.py
@@ -1,32 +1,25 @@
-# run one-to-one matching based on schedule
+# run one-to-one matching 
 
-# %%
+from pathlib import Path
+
 import pandas as pd
 import seaborn as sns
 
-from pathlib import Path
 from scipy.optimize import linear_sum_assignment
 
 # %%
 
-output_dir = "output"
+def team_mentor_pivots(df_chapter: pd.DataFrame):
 
-output_all = Path(output_dir) / "all_chapter_matches.csv"
-
-df = pd.read_csv(Path(output_dir) / "mentor_team_match_requests_clean.csv")
-
-# %%
-
-max_rank = df["rank"].max()
-
-all_chapter_matches = []
-
-for chapter, df_chapter in df.groupby("chapter"):
-
+    # pivot tables 
+    # team vs mentor, mentor vs team, with rankings as values
     team_df = df_chapter[df_chapter["type"] == "Team"]
     mentor_df = df_chapter[df_chapter["type"] == "Mentor"]
+    
     team_to_mentor = team_df.pivot(index="requester", columns="requestee", values="rank")
     mentor_to_team = mentor_df.pivot(index="requester", columns="requestee", values="rank")
+
+    # ensure all teams and mentors present in each table
     
     all_teams = pd.concat([team_df["requester"], mentor_df["requestee"]]).unique()
     all_mentors = pd.concat([team_df["requestee"], mentor_df["requester"]]).unique()
@@ -35,34 +28,39 @@ for chapter, df_chapter in df.groupby("chapter"):
     mentor_to_team = mentor_to_team.reindex(index=all_mentors, columns=all_teams)
 
     sns.heatmap(team_to_mentor, annot=team_to_mentor, fmt='.0f', cmap='YlGnBu')
-    # sns.heatmap(mentor_to_team)
 
     # get directionality
-    # 0 - unmatched, 1 - team to mentor only, 2 - mentor to team only, 3 - mutual
+    # 0 - unmatched, 1 - team pref, 2 - mentor pref, 3 - mutual
     pair_type = (
         team_to_mentor.notna().astype(int)
         + mentor_to_team.T.notna().astype(int) * 2
     )
     sns.heatmap(pair_type, annot=pair_type)
-    
-    # fill missing rows/columns with max ranking
-    t2m_filled = team_to_mentor.fillna(max_rank).astype(int)
-    m2t_filled = mentor_to_team.fillna(max_rank).astype(int)
 
+    return team_to_mentor, mentor_to_team, pair_type
+
+
+def get_matches(chapter_name, team_to_mentor, mentor_to_team, na_fill_value):
+
+    # fill missing rows/columns with fill value (max ranking?)
+    t2m_filled = team_to_mentor.fillna(na_fill_value).astype(int)
+    m2t_filled = mentor_to_team.fillna(na_fill_value).astype(int)
+
+    # "cost" matrix is the summed rankings, then minimize bipartite matching
     cost = t2m_filled + m2t_filled.T
-
     row_ind, col_ind = linear_sum_assignment(cost.values)
 
+    # construct output table
     matches = pd.DataFrame({
-        "chapter": chapter,
+        "chapter": chapter_name,
         "team": cost.index[row_ind],
         "mentor": cost.columns[col_ind],
         "mutual_rank_sum": cost.values[row_ind, col_ind],
+        "team_rank": team_to_mentor.to_numpy()[row_ind, col_ind],
+        "mentor_rank": mentor_to_team.to_numpy()[col_ind, row_ind]
     })
 
-    matches["team_rank"] = team_to_mentor.to_numpy()[row_ind, col_ind]
-    matches["mentor_rank"] = mentor_to_team.to_numpy()[col_ind, row_ind]
-
+    # store provenance of match reasons
     matches["match_type"] = (
         matches["team_rank"].notna().astype(int)
         + matches["mentor_rank"].notna().astype(int)
@@ -73,11 +71,39 @@ for chapter, df_chapter in df.groupby("chapter"):
         0: "unranked"
     })
 
-    all_chapter_matches.append(matches)
-
-
-all_chapter_matches = pd.concat(all_chapter_matches)
-
-all_chapter_matches.to_csv("all_chapter_matches.csv")
+    return matches
 
 # %%
+
+def main():
+
+    
+    output_dir = "output"
+
+    output_all = Path(output_dir) / "all_chapter_matches.csv"
+
+    df = pd.read_csv(Path(output_dir) / "mentor_team_match_requests_clean.csv")
+
+    max_rank = df["rank"].max()
+
+    all_chapter_matches = []
+
+    for chapter, df_chapter in df.groupby("chapter"):
+
+        print(f"Matching {chapter}")
+
+        team_to_mentor, mentor_to_team, pair_type = team_mentor_pivots(df_chapter)
+        matches = get_matches(
+            chapter, team_to_mentor, mentor_to_team, na_fill_value=max_rank
+            )
+        print(matches)
+
+        all_chapter_matches.append(matches)
+
+    all_chapter_matches = pd.concat(all_chapter_matches)
+
+    all_chapter_matches.to_csv(output_all)
+
+
+if __name__ == '__main__':
+    main()
